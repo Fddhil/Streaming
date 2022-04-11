93f683891d9a7a0d996022f5841ef4a102d490b0
Fddhil <103299943+Fddhil@users.noreply.github.com>
2022-04-11 08:05:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ client = commands.Bot(
 
 @client.event
 async def on_connect():
-  await client.change_presence(activity = discord.Streaming(name = " Fzzly ┊ SLOW RESPON ┊ Sedang Turu!!! ┊ 24/7 Online ┊ AFK ┊ https://discord.gg/ryHbUPTb ", url = "https://www.twitch.tv/fzzly_"))
+  await client.change_presence(activity = discord.Streaming(name = " Fzzly ┊ FAST RESPON! ┊ LIVE ON!!┊ 24/7 Online ┊ PLAYING GTA SAMP ┊ https://discord.gg/ryHbUPTb ", url = "https://www.twitch.tv/fzzly_"))
 
 
 keep_alive.keep_alive()
